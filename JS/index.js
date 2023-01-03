76ebc3d869556153ce182f713795fda56a7daea8
Odadobra <emmauagbale@gmail.com>
2023-01-03 23:04:18 +0000

    solved question 5, created new functions and added/removed comments

--- JS/index.js
+++ JS/index.js
@@ -137,14 +137,17 @@ console.log("Greatest Increase in Profits: " + calculateMax());
 
 //Greatest decrease in profit(date and amount) over the entire period
 let calculateMin = () => {
-    let min = 0;
-    for (let i = 0; i < finances.length; i++) {
-        if (Array.isArray(finances[i]) && finances[i + 1]) {
-            min += finances[i + 1][1] - finances[i][1];
-        };
-    };
-    return Math.min(min);
+    let minProfit = 0;
+    for (let i = 1; i < finances.length; i++) {
+        let profitDay = finances[i][1]
+        let previousDay = finances[i - 1][1]
+        let profit = profitDay - previousDay
+        if (profit < minProfit) {
+            minProfit = profit
+        }
+    }
+    return minProfit;
 };
-console.log("Greatest Decrease in Profits: " + calculateMin().toFixed(2).slice(1, 8));
-//196785
+
+console.log("Greatest Increase in Profits: " + calculateMin());
 //correct answer -2196167
